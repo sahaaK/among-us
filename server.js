7c8d9bb93fe2024632dfa99f93fd3c1074b0aee6
sahaaK <sahankaxogaha486@gmail.com>
2025-01-22 13:49:14 -0600

    Fix port binding issue

--- server.js
+++ server.js
@@ -83,4 +83,4 @@ io.on('connection', (socket) => {
 });
 
 // Start the server
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
